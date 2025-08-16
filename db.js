4f5c97e97759a86ab658cfc84e38a2c28fac9b50
lostintaha <tahaubaid921@gmail.com>
2025-08-16 16:50:47 +0500

    32

--- db.js
+++ db.js
@@ -1,14 +1,12 @@
-
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
-  port: 3307,
-  database: "team_management", 
+  port: 3306,
+  database: "team_management",
 });
-
 
 connection.connect((err) => {
   if (err) {
@@ -18,15 +16,8 @@ connection.connect((err) => {
   console.log(" Connected to the database successfully!");
 });
 
-const createMemeber = async (payload, res) => {
-  console.log("Creating member");
-  console.log(payload);
-
-  // if (!payload) {
-  //   return res.status(400).json({ error: "Invalid users data" });
-  // }
-
-  let query = "INSERT INTO users SET ?";
+const createMember = (payload, callback) => {
+  const query = "INSERT INTO users SET ?";
   const data = {
     name: payload.name,
     email: payload.email,
@@ -38,21 +29,26 @@ const createMemeber = async (payload, res) => {
   };
 
   connection.query(query, data, (err, result) => {
-    console.log(err, result);
+    if (err) {
+      console.error(" Error inserting member:", err);
+      return callback(err, null);
+    }
+    // callback(null, result.insertId);
   });
 };
-function getAllUsers(callback) {
+const getAllUsers = (callback) => {
   connection.query("SELECT * FROM users", (err, results) => {
     if (err) {
-      console.error("Error fetching users:", err);
+      console.error(" Error fetching users:", err);
       return callback(err, null);
     }
     callback(null, results);
   });
-}
+};
+//
 getAllUsers((err, usersData) => {
   if (!err) {
-    console.log( usersData);
+    console.log(usersData);
   }
 });
 const getUserByid = (id, callback) => {
@@ -65,9 +61,52 @@ const getUserByid = (id, callback) => {
     callback(null, results[0]);
   });
 };
-module.exports = { getUserByid };
+const updateMember = (id, data, callback) => {
+  const query = `
+    UPDATE users 
+    SET name=?, email=?, cnic=?, designation=?, joining=?, address=?, phone=? 
+    WHERE id=?
+  `;
+  const values = [
+    data.name,
+    data.email,
+    data.cnic,
+    data.designation,
+    data.joining,
+    data.address,
+    data.phone,
+    id,
+  ];
 
+  connection.query(query, values, (err, result) => {
+    if (err) {
+      console.error("Error updating user:", err);
+      return callback(err, null);
+    }
+    callback(null, result.affectedRows > 0);
+  });
+};
+const deleteMember = (id, callback) => {
+  const query = "DELETE FROM users WHERE id = ?";
+  connection.query(query, [id], (err, result) => {
+    if (err) {
+      console.error("Error deleting user:", err);
+      return callback(err, null);
+    }
+    callback(null, result.affectedRows > 0);
+  });
+};
 
-let gau = getAllUsers;
+module.exports = {
+  createMember,
+  getAllUsers,
+  getUserByid,
+  updateMember,
+  deleteMember,
+};
 
-exports.createMemeber = createMemeber;
+// module.exports = { getUserByid, createMember };
+
+// let gau = getAllUsers;
+
+// exports.createMember = createMember;
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require("./db");
 
 const server = http.createServer(requestHandler);
 
-const PORT = 3002;
+const PORT = 3006;
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
